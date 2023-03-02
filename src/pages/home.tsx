8de53ad06357b0ad2b39917c9ce8b554d1635128
Bingdongsanchi <chrisau418@gmail.com>
2023-03-01 22:10:53 -0800

    just changed to same colors again

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -22,13 +22,13 @@ const home = () => {
                 title="Properties for Sale"
                 value={684}
                 series={[75, 25]}
-                colors={['#475be8', '#e4e8ef']}
+                colors={['#275be8', '#c4e8ef']}
             />
             <PieChart 
                 title="Properties for Rent"
                 value={550}
                 series={[60, 40]}
-                colors={['#475ae8', '#e4b8ef']}
+                colors={['#275be8', '#c4e8ef']}
             />
             <PieChart 
                 title="Total customers"
@@ -40,7 +40,7 @@ const home = () => {
                 title="Properties for Cities"
                 value={555}
                 series={[75, 25]}
-                colors={['#475be8', '#e4e8ef']}
+                colors={['#275be8', '#c4e8ef']}
             />
         </Box>
 
