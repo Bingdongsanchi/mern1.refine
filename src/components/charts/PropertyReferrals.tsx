cd931cb38e10fe856dd40a35f3b35bf8dcb0e3bb
Bingdongsanchi <chrisau418@gmail.com>
2023-04-29 22:35:41 -0700

    done for tonight

--- src/components/charts/PropertyReferrals.tsx
+++ src/components/charts/PropertyReferrals.tsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@pankod/refine-mui'
-import { propertyReferralsInfo } from 'constants/index'
+import { propertyReferralsInfo } from 'constants/index';
+
+interface ProgressBarProps {
+  title: string,
+  percentage: number,
+  color: string,
+}
+const ProgressBar = ({title, percentage, color}
+  :ProgressBarProps) => (
+    <Box width="100%">
+      <Stack direction="row" 
+      alignItems="center"  
+      justifyContent="space-between">
+        <Typography fontSize={16} fontWeight={500} color="#11142d">
+                {title}
+            </Typography>
+            <Typography fontSize={16} fontWeight={500} color="#11142d">
+                {percentage}%
+            </Typography>
+      </Stack>
+    </Box>
+)
+
+
+
 const PropertyReferrals = () => {
   return (
     <Box 
@@ -15,6 +39,9 @@ const PropertyReferrals = () => {
         {600} color="#11142d">
             Property Referrals
         </Typography>
+        <Stack my="20px" direction="column" gap={4}>
+
+        </Stack>
 
     </Box>
   )
